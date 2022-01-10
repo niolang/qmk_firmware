46a9ba17f4d66158f0de70fcfb63c96451d4df0f
niolang <nlangroudi@gmail.com>
2022-01-10 20:27:41 +0100

    updating keylogr for bépo layout

--- keyboards/lily58/keymaps/bepo_niolang/keymap.c
+++ keyboards/lily58/keymaps/bepo_niolang/keymap.c
@@ -187,12 +187,22 @@ uint8_t  keylogs_str_idx        = 0;
 uint16_t log_timer              = 0;
 
 const char code_to_name[60] = {
+    ' ', ' ', ' ', ' ', 'a', 'k', 'x', 'i', 'p', 'e',
+    ',', 'c', 'd', 't', 's', 'r', 'q', ' ', 'l', 'j',
+    'b', 'o', 'u', 'e', 'v', '.', 'e', 'y', '^', 'a',
+    ' ', ' ', ' ', '(', ')', '@', '+', '-', '/', '*',
+    'R', 'E', 'B', 'T', '_', '-', '=', '[', ']', '\\',
+    '#', ';', '\'', '`', ',', '.', '/', ' ', ' ', ' '};
+
+/*
+const char code_to_name[60] = {
     ' ', ' ', ' ', ' ', 'a', 'b', 'c', 'd', 'e', 'f',
     'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p',
     'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
     '1', '2', '3', '4', '5', '6', '7', '8', '9', '0',
     'R', 'E', 'B', 'T', '_', '-', '=', '[', ']', '\\',
     '#', ';', '\'', '`', ',', '.', '/', ' ', ' ', ' '};
+*/
 
 void add_keylog(uint16_t keycode) {
     if ((keycode >= QK_MOD_TAP && keycode <= QK_MOD_TAP_MAX) || (keycode >= QK_LAYER_TAP && keycode <= QK_LAYER_TAP_MAX)) {
